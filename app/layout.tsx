f973f003e5b31252a0a41ec3981e98f7ad37209f
akshay.bandivadekar <c4.akshay.bandivadekar@gmail.com>
2024-12-18 00:04:56 +0530

    Added User Authentication using Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+
 import './globals.css';
-import Header from '@/components/Header';
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,16 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className}`}>
-        <Header />
-        <main className='min-h-screen'>{children}</main>
-        <footer className='bg-blue-50 py-12'>
-          <div className='container mx-auto px-4 text-center text-gray-600'>
-            <p>Made with &#x1F497; by Akshay</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className='min-h-screen'>{children}</main>
+          <footer className='bg-blue-50 py-12'>
+            <div className='container mx-auto px-4 text-center text-gray-600'>
+              <p>Made with &#x1F497; by Akshay</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='fixed top-0'>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Header;
